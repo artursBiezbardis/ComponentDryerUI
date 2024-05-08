b26aef08bbb0b073591f800ee2c5afc096860cd6
arturs.biezbardis <arturste2007@gmail.com>
2024-05-08 09:45:46 +0300

    ajustment for window size

--- view/app.py
+++ view/app.py
@@ -1,9 +1,11 @@
 from kivy.app import App
 from kivy.lang import Builder
 from view.mainLayout import MainLayout
+from kivy.core.window import Window
 
 
 class DryerApp(App):
     def build(self):
+        Window.size = (640, 480)
         Builder.load_file('kv_files/main.kv')
         return MainLayout()
