dad07b66e1830d75740a4552165abd9b04c2fc96
arturs.biezbardis <arturs.biezbardis@saftehnika.com>
2024-08-06 06:53:38 +0100

    update

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 SERIAL_SETTINGS = {
     'baud_rate': 9800,
     'timeout': 0.1,
-    'port': 'COM12',
+    'port': '/dev/tty',
     'stack_light_red_on': 'RED',
     'stack_light_green_on': 'GREEN',
     'stack_light_yellow_on': 'YELLOW',
