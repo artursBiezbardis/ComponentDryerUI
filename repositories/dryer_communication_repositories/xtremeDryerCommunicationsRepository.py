9e64e89f198ed3a8363a784f16efc84adebdc2ab
arturs.biezbardis <arturste2007@gmail.com>
2024-10-22 14:04:54 +0300

    fixed bug with reading data from dryer and fixed issue with add unregistred component

--- repositories/dryer_communication_repositories/xtremeDryerCommunicationsRepository.py
+++ repositories/dryer_communication_repositories/xtremeDryerCommunicationsRepository.py
@@ -12,9 +12,8 @@ class XtremeDryerCommunicationsRepository:
         expected_path = os.path.join(cwd, 'liveMonitorData.json')
         new_modified_time = os.path.getmtime(expected_path)
         time_now = time.time()
-        while config.TIMER_SETTINGS['json_read_lower_offset'] < (time_now - new_modified_time) > config.TIMER_SETTINGS['json_read_higher_offset']:
-            new_modified_time = os.path.getmtime(expected_path)
-            time_now = time.time()
+        if config.TIMER_SETTINGS['json_read_lower_offset'] < (time_now - new_modified_time) > config.TIMER_SETTINGS['json_read_higher_offset']:
+            time.sleep(1)
         with open(expected_path, 'r') as liveMonitorData:
             dryer_data_now = json.load(liveMonitorData)
 
