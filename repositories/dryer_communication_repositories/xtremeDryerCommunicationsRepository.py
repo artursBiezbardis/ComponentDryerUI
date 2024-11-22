cfcbc131ac71871cd3701fdea8789e72ad509328
arturs.biezbardis <arturste2007@gmail.com>
2024-11-22 13:33:25 +0200

    testing new functionality for reading live monitor data

--- repositories/dryer_communication_repositories/xtremeDryerCommunicationsRepository.py
+++ repositories/dryer_communication_repositories/xtremeDryerCommunicationsRepository.py
@@ -6,62 +6,36 @@ import config
 
 class XtremeDryerCommunicationsRepository:
 
+    def __init__(self):
+        self.cwd = os.getcwd()
+        self.expected_path = os.path.join(self.cwd, 'liveMonitorData.json')
+
+    def main(self):
+        dryer_data_now = {}
+        while not dryer_data_now:
+            time.sleep(0.3)
+            dryer_data_now = self.read_extracted_data()
+
+        return dryer_data_now
+
+    def read_extracted_data(self):
+        try:
+            with open(self.expected_path, 'r') as liveMonitorData:
+                dryer_data_now = json.load(liveMonitorData)
+        except Exception as e:
+            print(e)
+            return {}
+
+        return dryer_data_now
+
     @staticmethod
-    def main():
-        cwd = os.getcwd()
-        expected_path = os.path.join(cwd, 'liveMonitorData.json')
-        new_modified_time = os.path.getmtime(expected_path)
+    def get_seconds_and_deci_seconds() -> list:
+
         time_now = time.time()
         seconds = int(time_now)
         last_digit_sec = seconds % 10
         milliseconds = int((time_now - int(time_now)) * 1000)
         first_digit_milli_seconds = int(str(milliseconds)[0])
-        if last_digit_sec % 2:
-            if first_digit_milli_seconds <= 5:
-                try:
-                    with open(expected_path, 'r') as liveMonitorData:
-                        dryer_data_now = json.load(liveMonitorData)
-                except Exception as e:
-                    print(e)
-            else:
-                time.sleep(2.1 - float('0.' + f'{first_digit_milli_seconds}'))
-                try:
-                    with open(expected_path, 'r') as liveMonitorData:
-                        dryer_data_now = json.load(liveMonitorData)
-                except Exception as e:
-                    print(e)
-        else:
-            time.sleep(1)
-            if first_digit_milli_seconds <= 5:
-                try:
-                    with open(expected_path, 'r') as liveMonitorData:
-                        dryer_data_now = json.load(liveMonitorData)
-                except Exception as e:
-                    print(e)
-            else:
-                time.sleep(2.1 - float('0.' + f'{first_digit_milli_seconds}'))
-                try:
-                    with open(expected_path, 'r') as liveMonitorData:
-                        dryer_data_now = json.load(liveMonitorData)
-                except Exception as e:
-                    print(e)
 
-
-
-        # if config.TIMER_SETTINGS['json_read_lower_offset'] < (time_now - new_modified_time) > config.TIMER_SETTINGS['json_read_higher_offset']:
-        #     time.sleep(1.01)
-        # try:
-        #     with open(expected_path, 'r') as liveMonitorData:
-        #         dryer_data_now = json.load(liveMonitorData)
-        # except Exception as e:
-        #     if config.TIMER_SETTINGS['json_read_lower_offset'] < (time_now - new_modified_time) > config.TIMER_SETTINGS['json_read_higher_offset']:
-        #         time.sleep(1.01)
-        #     try:
-        #         with open(expected_path, 'r') as liveMonitorData:
-        #             dryer_data_now = json.load(liveMonitorData)
-        #     except Exception as e:
-        #
-        #        dryer_data_now = {'door1_closed': True, 'door2_closed': True, 'last_alarm': '', 'on_line': True, 'rh_value': 0.78, 'set_rh': 0.5, 'set_temp': 20.0, 'temp_value': 23.8, 'time': time_now}
-
-        return dryer_data_now
+        return [last_digit_sec, first_digit_milli_seconds]
 
